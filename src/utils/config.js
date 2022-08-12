f63ed9b0f42a4dc05948c85d593a98c37c98023f
Ivan <ivankuartz@hotmail.com>
2022-08-12 18:10:08 -0300

    Correct contract name

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || "${marketplacecontract.ikuartz.testnet}"; // line 1
+const CONTRACT_NAME = process.env.CONTRACT_NAME || "marketplacecontract.ikuartz.testnet"; // line 1
 
 function environment(env) {
   switch (env) {
